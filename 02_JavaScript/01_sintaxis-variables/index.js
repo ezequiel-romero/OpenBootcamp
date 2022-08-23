b66bc0c0410a6aad028184d0db80531dde4c6031
ezequiel-romero <eze.1003@gmail.com>
2022-08-23 15:17:22 -0300

    2.Sintaxis, variables y palabras reservadas de JS

---
+++ 02_JavaScript/01_sintaxis-variables/index.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Ezequiel",
+    32,
+    true,
+    Date(March, 10, 1990),
+    {
+        title: "La sombra del viento",
+        author: "Carlos Ruiz Zafón",
+        published: 2001,
+        url: "https://es.wikipedia.org/wiki/La_sombra_del_viento"
+    }
+]
