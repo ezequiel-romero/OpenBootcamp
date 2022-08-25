50d6901e252b44bbce04660f574d35bebcbfaa6f
ezequiel-romero <eze.1003@gmail.com>
2022-08-25 15:30:27 -0300

    7. Trabajando con Sets y Objetos - 1

---
+++ 02_JavaScript/06_sets-y-objetos/conjuntos.js
@@ -0,0 +1,11 @@
+// Un nuevo Set con los nombres de tu familia
+const family = new Set(['Juana', 'Carlos', 'Carolina', 'Lucas'])
+console.log(family)
+
+// Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+family.add('Lucas')
+console.log(family)
+
+// Modifica el Set original añadiendo el nombre "Javascript"
+family.add('Javascript')
+console.log(family)
