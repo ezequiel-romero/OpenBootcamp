757978dc4305ebb4322f1754ed59b97ea7fcf905
ezequiel-romero <eze.1003@gmail.com>
2022-08-29 12:29:42 -0300

    11. POO en JavaScript

---
+++ 02_JavaScript/10_poo/index.js
@@ -0,0 +1,14 @@
+class Estudiante {
+    nombre = "Ezequiel"
+    asignaturas = ["Javascript", "HTML", "CSS"]
+    obtenDatos() {
+        return {
+            nombre: this.nombre,
+            asignaturas: this.asignaturas
+        }
+    }
+}
+
+const nuevoEstudiante = new Estudiante()
+
+console.log(nuevoEstudiante.obtenDatos())
